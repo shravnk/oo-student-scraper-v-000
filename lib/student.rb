8358ff1057ac7dc553ea3f2c6251d90bd201ac31
Brendan O'Dea <brendan.ode4@gmail.com>
2018-02-23 01:27:59 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,7 +7,7 @@ class Student
   def initialize(student_hash)
     @name = student_hash[:name]
     @location = student_hash[:location]
-    @profile_url = attributes_hash[:profile_url]
+    @profile_url = student_hash[:profile_url]
     @@all << self
   end
 
